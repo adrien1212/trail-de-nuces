f518158bf8e336fdf0c7f2d9f5c7409591bd176b
adrien1212 <batman.caubel@laposte.net>
2026-02-15 18:21:47 +0100

    github action

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/marcillac-trail-joy/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/parcours" element={<Parcours />} />
